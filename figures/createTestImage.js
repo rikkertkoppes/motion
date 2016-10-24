ebb68c3ea9535e86685f310e570204f6a094c826
Rikkert Koppes <rikkert.koppes@theapsgroup.com>
2016-10-24 13:09:11 +0200

    colored test images

--- figures/createTestImage.js
+++ figures/createTestImage.js
@@ -30,25 +30,25 @@ while (s < data.points.length) {
     s += sliceSize;
 }
 
-slices.reduce(function(pending, slice, i) {
-    return pending.then(function() {
-        console.log(slice.length);
-        return renderPoints(slice,'./test/test'+i+'.png');
-    });
-}, Promise.resolve()).then(function() {
-    console.log('done');
-}, function(err) {
-    console.log(err);
-});
-
-
-
-// var points = data.points.slice(400,750);
-// renderPoints(points, './test.png').then(function() {
+// slices.reduce(function(pending, slice, i) {
+//     return pending.then(function() {
+//         console.log(slice.length);
+//         return renderPoints(slice,'./test/test'+i+'.png');
+//     });
+// }, Promise.resolve()).then(function() {
 //     console.log('done');
 // }, function(err) {
 //     console.log(err);
 // });
+
+
+
+var points = data.points.slice(400,750);
+renderPoints(points, './test.png').then(function() {
+    console.log('done');
+}, function(err) {
+    console.log(err);
+});
 
 function renderPoints(points, path) {
     var cfiltered = helpers.process(points, helpers.lowPass(0.1));
@@ -58,22 +58,49 @@ function renderPoints(points, path) {
 
 
     clear();
-    new paper.Path(wfiltered).set(defaultStyle);
+    createStroke(new paper.Path(wfiltered).set(defaultStyle), 4);
     paper.view.update();
 
     return renderImage(path, canvas);
 }
+
+
 
 function clear() {
     paper.project.activeLayer.removeChildren();
     paper.Path.Rectangle(new paper.Point([-20, -10]), new paper.Point([20, 10])).set({ fillColor: 'black' });
 }
 
+function createStroke(path, strokeWidth) {
+    //not at 0 and length, because the tangent is not calculated correctly at that point
+    createRectAt(path, 0.0001, strokeWidth);
+    for (var i=1; i< path.length; i++) {
+        createRectAt(path, i, strokeWidth);
+    }
+    createRectAt(path, path.length-0.0001, strokeWidth);
+    path.remove();
+    return path.set({
+        strokeWidth: strokeWidth
+    });
+}
+
+function createRectAt(path, offset, strokeWidth) {
+    var hw = strokeWidth;// / 2;
+    var heading = path.getTangentAt(offset).angle;
+    var color = {hue: heading, saturation: 1, brightness: 1};
+    paper.Path.Rectangle([-1.5,-hw],[1.5,hw]).set({
+        position: path.getPointAt(offset),
+        rotation: heading,
+        fillColor: color
+    });
+}
+
+
 //render to image
 function renderImage(path,canvas) {
     return new Promise(function(resolve,reject) {
         var out = fs.createWriteStream(path)
-        makeBinary(canvas);
+        // makeBinary(canvas);
 
         var stream = canvas.pngStream();
 
